cb0539b0c64aeea8b0b40a3ef367214302eac4bc
Sarah Giapitzakis <espeonblumbreon@gmail.com>
2016-07-04 22:29:20 +1000

    setup Unity project & began organising assets
    setup Unity project & began organising assets

---
+++ tiny-chao-garden-pc/Assets/src/Chao.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chao {
+
+    private enum ChaoType{
+        Empty,	
+        Egg,	
+        Child,	
+        Good,
+        Bad,
+        NeutralNormal,	
+        HeroNormal,
+        DarkNormal,
+        NeutralSwim,
+        HeroSwim,
+        DarkSwim,
+        NeutralFly,
+        HeroFly,
+        DarkFly,
+        NeutralRun,
+        HeroRun,
+        DarkRun,
+        NeutralPower,
+        HeroPower,
+        DarkPower,
+        NeutralChaos,
+        HeroChaos,
+        DarkChaos,
+        Tails,
+        Knuckles,
+        Amy
+    }
+
+    //basics
+    private string name; //0x12 18
+    private byte happiness; //0x82 130
+    //life
+    private short remainingLifespan1; //0x8A 138
+    private short remainingLifespan2; //0x8C 140
+    private short reincarnations; //0x8E 142
+    private byte chaoType; //0x80 128 ChaoType Enum
+
+
+
+    public Chao() { 
+    
+    }
+
+}
---
+++ tiny-chao-garden-pc/Assets/src/NewGame.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGame : MonoBehaviour {
+
+    void OnMouseDown() {
+        createNewGame();
+    }
+
+    void OnMouseOver() {
+
+    }
+
+    public void createNewGame(){ 
+        //initialize the game
+        Debug.Log("CREATING NEW GAME, PLEASE WAIT....");
+
+
+        //prepare a new chao egg	
+        Debug.Log("PREPARING NEW CHAO EGG, PLEASE WAIT....");
+
+
+        //go to chao_garden
+        Debug.Log("LOADING CHAO GARDEN, PLEASE WAIT....");
+        Application.LoadLevel("scene_chao_garden");
+
+        
+        //spawn new chao egg in garden
+        Debug.Log("SPAWNING NEW CHAO EGG, PLEASE WAIT....");
+
+    }
+
+}
