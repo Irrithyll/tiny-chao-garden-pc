00bda5b5ad4e355bdf175c2f926c8aa250620195
Sarah Giapitzakis <espeonblumbreon@gmail.com>
2016-07-13 20:26:19 +1000

    added prefabs for fruit
    added prefabs for fruit

---
+++ Assets/Resources/src/Fruit.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fruit : MonoBehaviour {
+
+    int maxSize = 3;
+    int currSize;
+
+	// Use this for initialization
+	void Start () {
+        currSize = maxSize;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void reduceSize()
+    {
+        currSize--;
+        if (currSize == 0)
+        {
+            //fruit has been eaten!
+        }
+
+    }
+
+
+}
